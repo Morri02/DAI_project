531acfe9d89d75fa351f7b8c8d00fc118536b24f
Morri02 <82643633+Morri02@users.noreply.github.com>
2025-01-14 17:38:19 +0100

    LamportMutex class created

---
+++ main.py
@@ -0,0 +1,124 @@
+from process import LamportProcess
+import time
+import random
+import json
+from threading import Thread
+import paho.mqtt.client as mqtt
+
+file_path = "config.json"
+with open(file_path, "r") as config_file:
+    d = json.load(config_file)
+
+BROKER = d["broker"]
+PORT = d["port"]
+TOPIC_REQUESTS = d["request_topic"]
+TOPIC_RELEASES = d["releases_topic"]
+TOPIC_ACKS = d["ack_topic"]
+TOPIC_HEART_BEAT = d["heart_beat_topic"]
+NUM_PROCESS = d["num_processes"]
+
+class Coordinator(Thread):
+    def __init__(self):
+        super().__init__()
+        self.num_processes = NUM_PROCESS
+        self.queue = []  # Lista di dizionari: {"timestamp": int, "process_id": int}
+        self.alive_processes = [False for _ in range(self.num_processes)]
+        self.dead_processes = [False for _ in range(self.num_processes)]
+
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id="observer")
+        self.client.on_message = self.on_message
+        self.client.connect(BROKER, PORT, 60)
+        self.client.subscribe([(TOPIC_REQUESTS, 0), (TOPIC_RELEASES, 0)])
+        self.client.loop_start()
+
+        self.stop_ = False
+        self.empty_queue = 0 #Number of consecutive times the queue was empty
+
+
+    def on_message(self, client, userdata, message):
+        topic = message.topic
+        payload = message.payload.decode("utf-8")
+        if topic == TOPIC_REQUESTS:
+            self.queue.append(json.loads(payload))
+            self.queue.sort(key=lambda x: (x["timestamp"], x["process_id"]))
+        elif topic == TOPIC_RELEASES:
+            self.queue = [elem for elem in self.queue if elem["process_id"] != json.loads(payload)["process_id"]]
+        elif topic == f"{TOPIC_HEART_BEAT}+":
+            process_id = int(topic.split("/")[-1])
+            self.alive_processes[process_id] = True
+            
+    
+    def stop(self):
+        self.stop_ = True
+    
+    def run(self):
+        while not self.stop_:
+            self.check_queue()
+            #self.check_processes()
+            time.sleep(2)
+    
+    def check_queue(self):
+        print("Lunghezza coda:", len(self.queue))
+        if not self.queue:
+            self.empty_queue += 1
+        else:
+            self.empty_queue = 0
+        if self.empty_queue >= 10:
+            self.stop()
+            print("Observer stopped.")
+            return
+    
+    def check_processes(self):
+        threads = []
+        self.alive_processes = [False for _ in range(self.num_processes)]
+        for process_id in range(self.num_processes):
+            t = Thread(target=self.check_process, args=(process_id,))
+            t.start()
+            threads.append(t)
+        
+        for t in threads:
+            t.join()
+        print(f"Alive processes: {[i for i, alive in enumerate(self.alive_processes) if alive]}")
+    
+
+    def check_process(self, process_id):
+        self.client.publish(f"{TOPIC_HEART_BEAT}{process_id}", "Alive?")
+        times = 0
+        while not self.alive_processes[process_id]:
+            time.sleep(1)
+            times += 1
+            if times >= 5:
+                self.dead_processes[process_id] = True
+                break
+
+class LamportMutex():
+    def __init__(self):
+        self.processes = [LamportProcess(i, NUM_PROCESS, broker=BROKER, port=PORT, log_file_path=f"log_files/{i}.log") for i in range(NUM_PROCESS)]
+        self.observer = Coordinator()
+    def start(self):
+        start = time.time()
+        self.observer.start()
+        for process in self.processes:
+            print("Starting process", process.process_id)
+            process.start()
+        
+        if self.observer.is_alive():
+            self.observer.join()
+            print("Stopping processes")
+            for process in self.processes:
+                process.stop()
+                process.join()
+        
+        for process in self.processes:
+            n = process.n
+            print(f"Process {process.process_id} acquired the resource {n} times out of the {process.num_operations} possible.")
+        
+        end = time.time()
+        elapsed_time = end - start
+        print(f"Execution time:{elapsed_time} seconds ({elapsed_time/60} minutes)")
+
+   
+if __name__ == "__main__":
+    mutex = LamportMutex()
+    
+    mutex.start()
---
+++ process.py
@@ -0,0 +1,143 @@
+import random
+import os
+import json
+import time
+from multiprocessing import Process
+from threading import Thread
+import paho.mqtt.client as mqtt
+
+
+file_path = "config.json"
+with open(file_path, "r") as config_file:
+    d = json.load(config_file)
+
+
+TOPIC_REQUESTS = d["request_topic"]
+TOPIC_RELEASES = d["releases_topic"]
+TOPIC_ACKS = d["ack_topic"]
+TOPIC_HEART_BEAT = d["heart_beat_topic"]
+
+class LamportProcess(Thread):
+    def __init__(self, process_id, num_process, broker="localhost", port=1883, log_file_path=None):
+        super().__init__()
+        self.process_id = process_id
+        self.num_processes = num_process
+        self.timestamp = 0
+        self.queue = []  # Lista di dizionari: {"timestamp": int, "process_id": int}
+        self.acks_received = [0 for _ in range(self.num_processes)]
+
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id=str(process_id))
+        self.client.on_message = self.on_message
+        #self.client.on_connect = self.on_connect
+        #self.client.on_disconnect = self.on_disconnect
+        self.client.connect(broker, port, 60)
+        self.client.subscribe([(TOPIC_REQUESTS, 0), (TOPIC_RELEASES, 0), (f"{TOPIC_ACKS}{self.process_id}", 0), (f"{TOPIC_HEART_BEAT}{self.process_id}", 0)])
+        self.client.loop_start()
+
+        self.stop_ = False
+        self.num_operations = 3
+
+        self.log_file_path = log_file_path
+        self.n = 0 #Number of time it acquired the resource
+
+        self.t = Thread(target=self.check_queue)
+        with open("queue.txt", "w") as f:
+            f.write(f"Processo{self.process_id} - Coda: {self.queue}\n")
+        self.t.start()
+
+    def check_queue(self):
+        while not self.stop_:
+            with open("queue.txt", "a") as f:
+                f.write(f"Processo {self.process_id} - Coda: {self.queue}\n")
+            time.sleep(0.5)
+
+    def run(self):
+        time.sleep(random.randint(1, 5))
+        for _ in range(self.num_operations):
+            if self.stop_:
+                break
+            self.do_something()
+
+    def stop(self):
+        self.stop_ = True
+        self.t.join()
+
+    def  do_something(self):
+        if random.uniform(0, 1) < 0.5: #Random behavior
+                self.get_resource()
+                self.perform_operation()
+                self.release_resource()
+    
+    def perform_operation(self):
+        time.sleep(2)
+
+    def on_connect(self, client, userdata, flags, rc, properties):
+        print(f"Processo {self.process_id} connesso al broker con codice di ritorno {rc}")
+
+    def on_disconnect(self, client, userdata, flags, rc, properties):
+        print(f"Processo {self.process_id} disconnesso dal broker con codice di ritorno {rc}")
+
+    def on_message(self, client, userdata, msg):
+        topic = msg.topic
+        if topic.startswith(TOPIC_HEART_BEAT):
+            if topic.startswith(f"{TOPIC_HEART_BEAT}{self.process_id}"):
+                self.client.publish(f"{TOPIC_HEART_BEAT}{self.process_id}", "yes", qos=2)
+            return
+        
+        payload = json.loads(msg.payload)
+
+        if topic == TOPIC_REQUESTS:
+            if payload["process_id"] != self.process_id:
+                self.timestamp = max(self.timestamp, payload['timestamp']) + 1
+                self.queue.append({"timestamp": payload['timestamp'], "process_id": payload['process_id']})
+                self.queue.sort(key=lambda x: (x["timestamp"], x["process_id"]))  # Ordina per timestamp e process_id
+                self.client.publish(f"{TOPIC_ACKS}{payload['process_id']}", json.dumps({"ack_from": self.process_id}), qos=2)
+
+        elif topic == TOPIC_RELEASES:
+            self.queue = [req for req in self.queue if req["process_id"] != payload['process_id']]
+            self.log(f"[+] {self.process_id}: Processo {payload['process_id']} ha rilasciato la risorsa")
+
+        elif topic.startswith(f"{TOPIC_ACKS}{self.process_id}"):
+            self.acks_received[payload['ack_from']] += 1
+
+
+    def get_resource(self):
+        self.timestamp += 1
+        self.client.publish(TOPIC_REQUESTS, json.dumps({"process_id": self.process_id, "timestamp": self.timestamp}))
+        self.queue.append({"timestamp": self.timestamp, "process_id": self.process_id})
+        
+        self.log(f"[+]Processo {self.process_id} richiede la risorsa")
+
+        start = time.perf_counter()
+        while self.acks_received.count(0) > 1 or (len(self.queue) > 0 and self.queue[0]["process_id"] != self.process_id):
+            time.sleep(0.1)
+            if self.log_file_path:
+                with open(self.log_file_path.split('.')[0] + "_wait.txt", "a") as f:
+                    if self.acks_received.count(0) > 1:
+                        acks_missing = [i for i, ack in enumerate(self.acks_received) if ack == 0]
+                        self.log(f"\tProcesso {self.process_id} in attesa di ACKs dei processi {acks_missing}")
+                    else:
+                        self.log(f"\tProcesso {self.process_id} in attesa di rilascio della risorsa da parte di {self.queue[0]['process_id']}")
+            if self.stop_:
+                self.release_resource()
+                return
+        end = time.perf_counter()
+
+        self.log(f"[+]Processo {self.process_id} ha acquisito la risorsa")
+        self.n += 1
+
+        if self.log_file_path:
+            with open(self.log_file_path, "a") as f:
+                f.write(f"Tempo di attesa del processo {self.client._client_id}: {end - start:.2f}\n")
+        
+    def release_resource(self):
+        #assert self.queue[0]["process_id"] == self.process_id
+        self.queue = [req for req in self.queue if req["process_id"] != self.process_id]
+        self.log(f"[-]Processo {self.process_id} rilascia la risorsa")
+        self.client.publish(TOPIC_RELEASES, json.dumps({"process_id": self.process_id}), qos=2)
+        self.acks_received = [0 for _ in range(self.num_processes)]
+        time.sleep(1)
+
+    def log(self, message):
+        with open(self.log_file_path, "a") as f:
+            f.write(message + "\n")
